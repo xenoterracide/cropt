b7cdc61ece6ee5c41fddf8afc4411115e90c35a3
Caleb Cushing <xenoterracide@gmail.com>
2009-04-30 22:57:49 -0400

    create schema definitions
    these can be installed into the db with psql -1 -f schema.sql

---
+++ sql/schema.sql
@@ -0,0 +1,14 @@
+-- I really hate these schema names.
+-- I may not like the design later either..
+--
+-- customer related data (some employees)
+   DROP SCHEMA IF EXISTS customer_data CASCADE;
+ CREATE SCHEMA customer_data;
+
+-- inventory / services (all employees / some public)
+   DROP SCHEMA IF EXISTS inv_svc CASCADE;
+ CREATE SCHEMA inv_svc;
+
+-- employee data (management only)
+   DROP SCHEMA IF EXISTS employees;
+ CREATE SCHEMA employees;
