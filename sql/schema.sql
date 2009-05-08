90b64592d1b4ad521236e4991bdafede97cae7ff
Caleb Cushing <xenoterracide@gmail.com>
2009-05-08 01:14:22 -0400

    modify main schema to be called consumers

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,8 +1,8 @@
 -- vim: ts=8
 -- after creating the database and correspoding user/pass create tables with
 -- psql -1f schema.sql
-   DROP SCHEMA IF EXISTS oc CASCADE;
- CREATE SCHEMA oc;
+   DROP SCHEMA IF EXISTS consumer CASCADE;
+ CREATE SCHEMA consumer;
 
 -- the following are skeletal domains
 -- they should be expanded in the future to carry stricter validation
@@ -10,27 +10,44 @@
  CREATE DOMAIN phone_t AS CHAR(14);
  CREATE DOMAIN phone_ext_t AS CHAR(4);
  CREATE DOMAIN state_t AS CHAR(2);
- CREATE DOMAIN vin_t AS CHAR(17); --vehicle identification number
  CREATE	DOMAIN email_t AS VARCHAR(255);
 
-/* CUSTOMERS ************************************************************/
- CREATE TABLE oc.customers (
+/* CONSUMERS ************************************************************/
+ CREATE TABLE consumer.consumers (
 	id	BIGINT		NOT NULL PRIMARY KEY,
 	lname	VARCHAR(255)	NOT NULL,
-	fname	VARCHAR(255)	NOT NULL,
+	fname	VARCHAR(255)	NOT NULL
+);
+ CREATE SEQUENCE consumer.consumer_id OWNED BY consumer.consumers.id;
+
+-- set default consumer.consumer.id to sequence
+  ALTER	TABLE consumer.consumers
+  ALTER COLUMN id
+    SET DEFAULT nextval('consumer.consumer_id');
+
+-- following tables split for normalizaton
+ CREATE TABLE consumer.phones (
+	id	BIGINT		NOT NULL
+		REFERENCES consumer.consumers (id)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE,
+	phone	phone_t
+);
+ CREATE TABLE consumer.addresses (
+	id	BIGINT		NOT NULL
+		REFERENCES consumer.consumers (id)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE,
 	addr1	VARCHAR(255),
 	addr2	VARCHAR(255),
-	zip	zip_t,
 	city	VARCHAR(255),
 	state	state_t,
+	zip	zip_t
+);
+ CREATE TABLE consumer.email (
+	id	BIGINT		NOT NULL
+		REFERENCES consumer.consumers (id)
+		ON UPDATE CASCADE
+		ON DELETE CASCADE,
 	email	email_t
 );
- CREATE SEQUENCE oc.customer_id OWNED BY oc.customers.id;
-
--- set default oc.customer.id to sequence
-  ALTER	TABLE oc.customers
-  ALTER COLUMN id
-    SET DEFAULT nextval('oc.customer_id');
-/************************************************************* CUSTOMERS */
-
-/* VEHICLES **************************************************************/
