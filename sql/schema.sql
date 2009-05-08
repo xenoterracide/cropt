5f0bbac7dd25c4522809735d57c4d2c6fdc3ef81
Caleb Cushing <xenoterracide@gmail.com>
2009-05-07 23:03:32 -0400

    rename customer table customers

--- sql/schema.sql
+++ sql/schema.sql
@@ -5,8 +5,8 @@
  CREATE SCHEMA oc;
 
  
-/* CUSTOMER *************************************************************/
- CREATE TABLE oc.customer (
+/* CUSTOMERS ************************************************************/
+ CREATE TABLE oc.customers (
 	id	BIGINT		NOT NULL PRIMARY KEY,
 	lname	VARCHAR(255)	NOT NULL,
 	fname	VARCHAR(255)	NOT NULL,
@@ -16,10 +16,12 @@
 	state	CHAR(2),
 	phone	VARCHAR(10) -- might be short
 );
- CREATE SEQUENCE oc.customer_id OWNED BY oc.customer.id;
+ CREATE SEQUENCE oc.customer_id OWNED BY oc.customers.id;
 
 -- set default oc.customer.id to sequence
-  ALTER	TABLE oc.customer
+  ALTER	TABLE oc.customers
   ALTER COLUMN id
     SET DEFAULT nextval('oc.customer_id');
-/************************************************************** CUSTOMER */
+/************************************************************* CUSTOMERS */
+
+/* VEHICLES **************************************************************/
