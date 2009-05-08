fbc3d5238bdccc4828b5810bf63b4de5742e6aba
Caleb Cushing <xenoterracide@gmail.com>
2009-05-07 22:50:13 -0400

    finish customer table

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,7 +1,23 @@
+-- vim: ts=8
+-- after creating the database and correspoding user/pass create tables with
+-- psql -1f schema.sql
    DROP SCHEMA IF EXISTS oc CASCADE;
  CREATE SCHEMA oc;
 
+ 
  CREATE TABLE oc.customer (
-	id	BIGINT NOT NULL PRIMARY KEY,
-	lname	VARCHAR(255) NOT NULL,
-	fname	VARCHAR(255) NOT NULL, 
+	id	BIGINT		NOT NULL PRIMARY KEY,
+	lname	VARCHAR(255)	NOT NULL,
+	fname	VARCHAR(255)	NOT NULL,
+	addr1	VARCHAR(255),
+	addr2	VARCHAR(255),
+	city	VARCHAR(255),
+	state	CHAR(2),
+	phone	VARCHAR(10) -- might be short
+);
+ CREATE SEQUENCE oc.customer_id OWNED BY oc.customer.id;
+
+-- set default oc.customer.id to sequence
+  ALTER	TABLE oc.customer
+  ALTER COLUMN id
+    SET DEFAULT nextval('oc.customer_id');
