3aaff87b72996af1cfff23295861c1f5a0550134
Caleb Cushing <xenoterracide@gmail.com>
2009-05-14 01:32:01 -0400

    port my test app into cropt

--- main.cpp
+++ main.cpp
@@ -1,10 +1,44 @@
-#include <QtGui/QApplication>
-#include "mainwindow.h"
+ #include <QtGui>
+ #include <QtSql>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
-}
+ #include "connection.h"
+
+ void initializeModel(QSqlRelationalTableModel *model)
+ {
+     model->setTable("consumer.consumers");
+
+     model->setEditStrategy(QSqlTableModel::OnManualSubmit);
+	 model->setRelation(0, QSqlRelation("consumer.phones", "id", "phone"));
+
+     model->setHeaderData(0, Qt::Horizontal, QObject::tr("ID"));
+     model->setHeaderData(2, Qt::Horizontal, QObject::tr("First Name"));
+     model->setHeaderData(1, Qt::Horizontal, QObject::tr("Last Name"));
+     model->setHeaderData(4, Qt::Horizontal, QObject::tr("Phone"));
+
+     model->select();
+ }
+
+ QTableView *createView(const QString &title, QSqlTableModel *model)
+ {
+     QTableView *view = new QTableView;
+     view->setModel(model);
+     view->setItemDelegate(new QSqlRelationalDelegate(view));
+     view->setWindowTitle(title);
+     return view;
+ }
+
+ int main(int argc, char *argv[])
+ {
+     QApplication app(argc, argv);
+     if (!createConnection())
+         return 1;
+
+     QSqlRelationalTableModel model;
+
+     initializeModel(&model);
+
+     QTableView *view = createView(QObject::tr("Relational Table Model"), &model);
+     view->show();
+
+     return app.exec();
+ }
---
+++ sql/test_data.sql
@@ -0,0 +1,25 @@
+-- vim: ts=8
+ INSERT INTO consumer.consumers
+	( fname, lname )
+ VALUES
+	( 'Caleb', 'Cushing' ),
+	( 'Amanda', 'Cobb' ),
+	( 'Kris', 'Richardson' )
+;
+ INSERT INTO consumer.phones
+	( id, phone )
+ VALUES
+	( 1, '15172305470' )
+;
+ INSERT INTO consumer.addresses
+	( id, addr1, addr2, city, state, zip )
+ VALUES
+	( 1, '8128 Clay Ct', NULL, 'Sterling Heights', 'MI', '48313' )
+;
+ INSERT INTO consumer.email
+	( id, email )
+ VALUES
+	( 1, 'xenoterracide@gmail.com'),
+	( 2, 'cscififan@yahoo.com'),
+	( 3, 'krich87@aol.com')
+;
