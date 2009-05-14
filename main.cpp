8d18fb651a71a84293e8617cf70541b6efd9c889
Caleb Cushing <xenoterracide@gmail.com>
2009-05-14 01:35:09 -0400

    fix whitespace

--- main.cpp
+++ main.cpp
@@ -1,44 +1,44 @@
- #include <QtGui>
- #include <QtSql>
+#include <QtGui>
+#include <QtSql>
 
- #include "connection.h"
+#include "connection.h"
 
- void initializeModel(QSqlRelationalTableModel *model)
- {
-     model->setTable("consumer.consumers");
+void initializeModel(QSqlRelationalTableModel *model)
+{
+	model->setTable("consumer.consumers");
 
-     model->setEditStrategy(QSqlTableModel::OnManualSubmit);
-	 model->setRelation(0, QSqlRelation("consumer.phones", "id", "phone"));
+	model->setEditStrategy(QSqlTableModel::OnManualSubmit);
+	model->setRelation(0, QSqlRelation("consumer.phones", "id", "phone"));
 
-     model->setHeaderData(0, Qt::Horizontal, QObject::tr("ID"));
-     model->setHeaderData(2, Qt::Horizontal, QObject::tr("First Name"));
-     model->setHeaderData(1, Qt::Horizontal, QObject::tr("Last Name"));
-     model->setHeaderData(4, Qt::Horizontal, QObject::tr("Phone"));
+	model->setHeaderData(0, Qt::Horizontal, QObject::tr("ID"));
+	model->setHeaderData(2, Qt::Horizontal, QObject::tr("First Name"));
+	model->setHeaderData(1, Qt::Horizontal, QObject::tr("Last Name"));
+	model->setHeaderData(4, Qt::Horizontal, QObject::tr("Phone"));
 
-     model->select();
- }
+	model->select();
+}
 
- QTableView *createView(const QString &title, QSqlTableModel *model)
- {
-     QTableView *view = new QTableView;
-     view->setModel(model);
-     view->setItemDelegate(new QSqlRelationalDelegate(view));
-     view->setWindowTitle(title);
-     return view;
- }
+QTableView *createView(const QString &title, QSqlTableModel *model)
+{
+	QTableView *view = new QTableView;
+	view->setModel(model);
+	view->setItemDelegate(new QSqlRelationalDelegate(view));
+	view->setWindowTitle(title);
+	return view;
+}
 
- int main(int argc, char *argv[])
- {
-     QApplication app(argc, argv);
-     if (!createConnection())
-         return 1;
+int main(int argc, char *argv[])
+{
+	QApplication app(argc, argv);
+	if (!createConnection())
+	    return 1;
 
-     QSqlRelationalTableModel model;
+	QSqlRelationalTableModel model;
 
-     initializeModel(&model);
+	initializeModel(&model);
 
-     QTableView *view = createView(QObject::tr("Relational Table Model"), &model);
-     view->show();
+	QTableView *view = createView(QObject::tr("Relational Table Model"), &model);
+	view->show();
 
-     return app.exec();
- }
+	return app.exec();
+}
